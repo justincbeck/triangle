3ec9cfda9c0ac2fc6cc53a33c3a5671a7e35c5bb
Justin Beck <justinbeck@mac.com>
2011-08-15 15:22:30 -0400

    Initial and final checkin

---
+++ lib/triangle.rb
@@ -0,0 +1,1 @@
+require 'triangle_sum/triangle'
---
+++ lib/triangle_sum/triangle.rb
@@ -0,0 +1,23 @@
+require 'enumerator'
+
+module TriangleSum
+    class Triangle
+        def calculate
+            file = File.expand_path(File.dirname(__FILE__)) + "/../../data/triangle.txt" 
+            rows = IO.readlines(file).collect { |l| l.split(' ').collect { |s| s.to_i } }
+
+            while rows.length > 1
+                row, next_row = rows.pop, rows.pop
+                tuple_row, new_row = Array.new, Array.new
+    
+                row.each_cons(2) { |a, b| tuple_row << [a, b] }
+                (next_row.zip tuple_row).each { |node| new_row << node[0] + node[1].max }
+    
+                rows.push new_row
+            end
+
+            puts rows
+        end
+    end
+end
+
